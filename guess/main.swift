baee5818933aad0daeb02d883534af628c65cf18
Jack Palevich <jack.palevich@gmail.com>
2017-12-25 11:50:15 -0800

    Initial Commit

---
+++ guess/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  guess
+//
+//  Created by Jack Palevich on 12/25/17.
+//  Copyright © 2017 Jack Palevich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
